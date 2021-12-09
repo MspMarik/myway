94047c30d9c4a794ca004ccfeaf990817156e912
MspMarik <msp3472@gmail.com>
2021-12-08 23:39:38 -0500

    done

--- public/js/index.js
+++ public/js/index.js
@@ -45,189 +45,188 @@ function drawChart() {
     chart = new google.visualization.PieChart(document.getElementById("piechart2"));
     chart.draw(data, options);
 }
-$(function () {
-    /*
+
+/*
         Clientside Error Handling Functions
     */
-    function validUsername(username) {
-        //Makes sure that all inputted usernames are at least four characters, and only contain alphanumeric input
-        let trimmedUsername = username.trim();
-        if (trimmedUsername.length < 4) {
+function validUsername(username) {
+    //Makes sure that all inputted usernames are at least four characters, and only contain alphanumeric input
+    let trimmedUsername = username.trim();
+    if (trimmedUsername.length < 4) {
+        return false;
+    }
+    for (let i = 0; i < trimmedUsername.length; i++) {
+        let isAlphabetic = trimmedUsername[i] >= "A" && trimmedUsername[i] <= "z";
+        let isNumeric = trimmedUsername[i] >= "0" && trimmedUsername[i] <= "9";
+        if (!isAlphabetic && !isNumeric) {
             return false;
         }
-        for (let i = 0; i < trimmedUsername.length; i++) {
-            let isAlphabetic = trimmedUsername[i] >= "A" && trimmedUsername[i] <= "z";
-            let isNumeric = trimmedUsername[i] >= "0" && trimmedUsername[i] <= "9";
-            if (!isAlphabetic && !isNumeric) {
-                return false;
-            }
-        }
-        return true;
     }
+    return true;
+}
 
-    function validPassword(password) {
-        //Makes sure that all inputted passwords are at least six characters, and don't contain any spaces
-        let trimmedPassword = password.trim();
-        if (trimmedPassword.length < 6) {
+function validPassword(password) {
+    //Makes sure that all inputted passwords are at least six characters, and don't contain any spaces
+    let trimmedPassword = password.trim();
+    if (trimmedPassword.length < 6) {
+        return false;
+    }
+    for (let i = 0; i < trimmedPassword.length; i++) {
+        if (trimmedPassword[i] == " ") {
             return false;
         }
-        for (let i = 0; i < trimmedPassword.length; i++) {
-            if (trimmedPassword[i] == " ") {
+    }
+    return true;
+}
+
+let errorDiv = document.getElementById("error");
+
+function errorOnPage(errString) {
+    errorDiv.hidden = false;
+    errorDiv.innerHTML = errString;
+}
+
+//Used in both login and signup
+let usernameInput = document.getElementById("username");
+let passwordInput = document.getElementById("password");
+
+//Login Form
+let loginForm = document.getElementById("login-form");
+if (loginForm) {
+    loginForm.addEventListener("submit", (event) => {
+        // event.preventDefault();
+        let providedUsername = usernameInput.value;
+        if (!providedUsername) {
+            errorOnPage("Username not provided");
+            return false;
+        }
+        if (typeof providedUsername != "string") {
+            errorOnPage("Username must be a string");
+            return false;
+        }
+
+        let providedPassword = passwordInput.value;
+        if (!providedPassword) {
+            errorOnPage("Password not provided");
+            return false;
+        }
+        if (typeof providedPassword != "string") {
+            errorOnPage("Password must be a string");
+            return false;
+        }
+
+        errorDiv.hidden = true;
+        //TODO: Find out how to post
+        let loginPOST = {
+            method: "POST",
+            url: "/loginlogout",
+            data: { username: providedUsername, password: providedPassword },
+        };
+        $.ajax(loginPOST);
+    });
+}
+
+//Signup Form
+let signupForm = document.getElementById("signup-form");
+let password2Input = document.getElementById("password2");
+if (signupForm) {
+    signupForm.addEventListener("submit", (event) => {
+        // event.preventDefault();
+        let providedUsername = usernameInput.value;
+        if (!providedUsername) {
+            errorOnPage("Username not provided");
+            return false;
+        }
+        if (typeof providedUsername != "string") {
+            errorOnPage("Username must be a string");
+            return false;
+        }
+        let trimmedUsername = providedUsername.trim();
+        if (!validUsername(trimmedUsername)) {
+            errorOnPage("Username must be at least four characters long, and only contain letters and numbers.");
+        }
+
+        let providedPassword = passwordInput.value;
+        if (!providedPassword) {
+            errorOnPage("Password not provided");
+            return false;
+        }
+        if (typeof providedPassword != "string") {
+            errorOnPage("Password must be a string");
+            return false;
+        }
+        let trimmedPassword = providedPassword.trim();
+        if (!validPassword(cleanedPassword)) {
+            errorOnPage("Password must be at least six characters long, and not contain any spaces.");
+        }
+
+        let providedPassword2 = password2Input.value;
+        if (!providedPassword2) {
+            errorOnPage("Please repeat your password");
+            return false;
+        }
+        if (typeof providedPassword != "string") {
+            errorOnPage("Repeated password must be a string");
+            return false;
+        }
+        let trimmedPassword2 = providedPassword2.trim();
+        if (trimmedPassword != trimmedPassword2) {
+            errorOnPage("Passwords don't match");
+            return false;
+        }
+
+        errorDiv.hidden = true;
+        //TODO: Find out how to post
+        let signupPOST = {
+            method: "POST",
+            url: "/signup",
+            data: { username: trimmedUsername, password: trimmedPassword, password2: trimmedPassword2 },
+        };
+        $.ajax(signupPOST);
+    });
+}
+
+//Search Form
+let searchForm = document.getElementById("search-form");
+let searchBox = document.getElementById("searchbox");
+let productionYear = document.getElementById("prodYear");
+if (searchForm) {
+    searchForm.addEventListener("submit", (event) => {
+        // event.preventDefault();
+        let searchTerm = searchBox.value;
+        if (!searchTerm) {
+            errorOnPage("Search term not provided");
+            return false;
+        }
+        if (typeof request.body.searchbox != "string") {
+            errorOnPage("Search term must be a string");
+            return false;
+        }
+        let trimmedSearchTerm = searchTerm.trim();
+        if (trimmedSearchTerm == "") {
+            errorOnPage("Search term connot be empty");
+            return false;
+        }
+        let year = productionYear.value;
+        if (year) {
+            if (typeof year != "number") {
+                errorOnPage("If provided, year should be a number");
                 return false;
             }
         }
-        return true;
-    }
 
-    let errorDiv = document.getElementById("error");
+        errorDiv.hidden = true;
+        //TODO: Find out how to post
+        let tag;
+        if (year) {
+            tag = year.toString();
+        }
 
-    function errorOnPage(errString) {
-        errorDiv.hidden = false;
-        errorDiv.innerHTML = errString;
-    }
-
-    //Used in both login and signup
-    let usernameInput = document.getElementById("username");
-    let passwordInput = document.getElementById("password");
-
-    //Login Form
-    let loginForm = document.getElementById("login-form");
-    if (loginForm) {
-        loginForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            let providedUsername = usernameInput.val();
-            if (!providedUsername) {
-                errorOnPage("Username not provided");
-                return false;
-            }
-            if (typeof providedUsername != "string") {
-                errorOnPage("Username must be a string");
-                return false;
-            }
-
-            let providedPassword = passwordInput.val();
-            if (!providedPassword) {
-                errorOnPage("Password not provided");
-                return false;
-            }
-            if (typeof providedPassword != "string") {
-                errorOnPage("Password must be a string");
-                return false;
-            }
-
-            errorDiv.hidden = true;
-            //TODO: Find out how to post
-            let loginPOST = {
-                method: "POST",
-                url: "/loginlogout",
-                data: { username: providedUsername, password: providedPassword },
-            };
-            $.ajax(loginPOST);
-        });
-    }
-
-    //Signup Form
-    let signupForm = document.getElementById("signup-form");
-    let password2Input = document.getElementById("password2");
-    if (signupForm) {
-        signupForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            let providedUsername = usernameInput.val();
-            if (!providedUsername) {
-                errorOnPage("Username not provided");
-                return false;
-            }
-            if (typeof providedUsername != "string") {
-                errorOnPage("Username must be a string");
-                return false;
-            }
-            let trimmedUsername = providedUsername.trim();
-            if (!validUsername(trimmedUsername)) {
-                errorOnPage("Username must be at least four characters long, and only contain letters and numbers.");
-            }
-
-            let providedPassword = passwordInput.val();
-            if (!providedPassword) {
-                errorOnPage("Password not provided");
-                return false;
-            }
-            if (typeof providedPassword != "string") {
-                errorOnPage("Password must be a string");
-                return false;
-            }
-            let trimmedPassword = providedPassword.trim();
-            if (!validPassword(cleanedPassword)) {
-                errorOnPage("Password must be at least six characters long, and not contain any spaces.");
-            }
-
-            let providedPassword2 = password2Input.val();
-            if (!providedPassword2) {
-                errorOnPage("Please repeat your password");
-                return false;
-            }
-            if (typeof providedPassword != "string") {
-                errorOnPage("Repeated password must be a string");
-                return false;
-            }
-            let trimmedPassword2 = providedPassword2.trim();
-            if (trimmedPassword != trimmedPassword2) {
-                errorOnPage("Passwords don't match");
-                return false;
-            }
-
-            errorDiv.hidden = true;
-            //TODO: Find out how to post
-            let signupPOST = {
-                method: "POST",
-                url: "/signup",
-                data: { username: trimmedUsername, password: trimmedPassword, password2: trimmedPassword2 },
-            };
-            $.ajax(signupPOST);
-        });
-    }
-
-    //Search Form
-    let searchForm = document.getElementById("search-form");
-    let searchBox = document.getElementById("searchbox");
-    let productionYear = document.getElementById("prodYear");
-    if (searchForm) {
-        searchForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            let searchTerm = searchBox.val();
-            if (!searchTerm) {
-                errorOnPage("Search term not provided");
-                return false;
-            }
-            if (typeof request.body.searchbox != "string") {
-                errorOnPage("Search term must be a string");
-                return false;
-            }
-            let trimmedSearchTerm = searchTerm.trim();
-            if (trimmedSearchTerm == "") {
-                errorOnPage("Search term connot be empty");
-                return false;
-            }
-            let year = productionYear.val();
-            if (year) {
-                if (typeof year != "number") {
-                    errorOnPage("If provided, year should be a number");
-                    return false;
-                }
-            }
-
-            errorDiv.hidden = true;
-            //TODO: Find out how to post
-            let tag;
-            if (year) {
-                tag = year.toString();
-            }
-
-            let searchPOST = {
-                method: "POST",
-                url: searchForm.action,
-                data: { searchbox: trimmedSearchTerm, tag: tag },
-            };
-            $.ajax(searchPOST);
-        });
-    }
-});
+        let searchPOST = {
+            method: "POST",
+            url: searchForm.action,
+            data: { searchbox: trimmedSearchTerm, tag: tag },
+        };
+        $.ajax(searchPOST);
+    });
+}
